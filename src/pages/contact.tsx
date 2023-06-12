a1893e78318427d776fba1e50200e6b5b7bcaf1d
Thomas <tdmac1999@gmail.com>
2023-06-12 15:25:48 -0400

    implemented carousel

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { type NextPage } from 'next';
+import PageHeader from '~/components/text/PageHeader';
 
 const Contact: NextPage = () => (
-  <div>Contact</div>
+  <div style={{ marginLeft: '10%' }} className="w-4/5 pt-28 mb-20">
+    <PageHeader>Contact</PageHeader>
+  </div>
 );
 
 export default Contact;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { type NextPage } from 'next';
 
+import p1 from 'src/assets/images/project_photos/personal site (1).png';
+import p2 from 'src/assets/images/project_photos/personal site (2).png';
+import p3 from 'src/assets/images/project_photos/personal site (3).png';
+import PageHeader from '~/components/text/PageHeader';
+import Carousel from '~/components/display/Carousel';
+
 const Projects: NextPage = () => (
-  <div>Projects</div>
+  <div style={{ marginLeft: '10%' }} className="w-4/5 pt-28 mb-20">
+    <PageHeader>Projects</PageHeader>
+    <Carousel images={[p1, p2, p3]} width="700px" />
+  </div>
 );
 
 export default Projects;
